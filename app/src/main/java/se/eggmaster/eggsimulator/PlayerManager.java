b073acb40b30a8697228fdd1841b8c1788f54f1b
f3lixz <felix.ponten@hotmail.se>
2016-07-29 15:01:26 +0200

    asd

---
+++ app/src/main/java/se/eggmaster/eggsimulator/PlayerManager.java
@@ -0,0 +1,46 @@
+package se.eggmaster.eggsimulator;
+
+import android.app.Activity;
+import android.content.SharedPreferences;
+
+import com.google.gson.Gson;
+
+import se.eggmaster.eggsimulator.Models.Player;
+
+/**
+ * Created by FelixTheMaster on 2016-07-29.
+ */
+public class PlayerManager {
+
+    private static final String PREFERENCE_NAME = "PlayerPrefsEggSim";
+    private static final String PLAYER_NAME = "PlayerNameEggSim";
+
+    private SharedPreferences mPrefs;
+    private Player mPlayer;
+
+    public PlayerManager(Activity activity) {
+        mPrefs = activity.getSharedPreferences(PREFERENCE_NAME, 0);
+        String playerJson = mPrefs.getString(PLAYER_NAME, "");
+        if (playerJson.isEmpty())
+            mPlayer = new Player();
+        else {
+            mPlayer = new Gson().fromJson(playerJson, Player.class);
+        }
+    }
+
+    public void updateLevel(int level) {
+        mPlayer.setLevel(level);
+        saveStorage();
+    }
+
+    public Player getPlayer() {
+        return mPlayer;
+    }
+
+    private void saveStorage() {
+        Gson gson = new Gson();
+        String player = gson.toJson(mPlayer);
+        mPrefs.edit().putString(PLAYER_NAME, player).commit();
+    }
+
+}
