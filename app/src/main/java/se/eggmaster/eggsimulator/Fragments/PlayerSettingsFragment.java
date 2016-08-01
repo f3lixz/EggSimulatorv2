9a2fe4cf3db4079a2ee61ac7d63d1c12f82ca140
f3lixz <felix.ponten@hotmail.se>
2016-08-01 17:30:19 +0200

    squashed eggchooser into player settings fragment

--- app/src/main/java/se/eggmaster/eggsimulator/Fragments/PlayerSettingsFragment.java
+++ app/src/main/java/se/eggmaster/eggsimulator/Fragments/PlayerSettingsFragment.java
@@ -7,6 +7,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import se.eggmaster.eggsimulator.MainActivity;
 import se.eggmaster.eggsimulator.R;
@@ -20,6 +22,10 @@ public class PlayerSettingsFragment extends Fragment implements View.OnClickList
     private LevelChooserWidget mLevelChooserWidget;
     private Button mContinueButton;
 
+    private ImageView mEgg2km;
+    private ImageView mEgg5km;
+    private ImageView mEgg10km;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -32,11 +38,37 @@ public class PlayerSettingsFragment extends Fragment implements View.OnClickList
         mLevelChooserWidget = (LevelChooserWidget) view.findViewById(R.id.levelChooser);
         mContinueButton = (Button) view.findViewById(R.id.continueButton);
         mContinueButton.setOnClickListener(this);
+        mEgg2km = (ImageView) view.findViewById(R.id.egg1);
+        mEgg2km.setOnClickListener(getEggOnClickListener());
+        mEgg5km = (ImageView) view.findViewById(R.id.egg2);
+        mEgg5km.setOnClickListener(getEggOnClickListener());
+        mEgg10km = (ImageView) view.findViewById(R.id.egg3);
+        mEgg10km.setOnClickListener(getEggOnClickListener());
+    }
+
+    public View.OnClickListener getEggOnClickListener() {
+        return new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                updateEggDrawables();
+                v.setSelected(true);
+                v.setBackground(getResources().getDrawable(R.drawable.selected_border));
+                v.invalidate();
+            }
+        };
+    }
+
+    private void updateEggDrawables() {
+        mEgg2km.setSelected(false);
+        mEgg5km.setSelected(false);
+        mEgg10km.setSelected(false);
+        mEgg2km.setBackground(null);
+        mEgg5km.setBackground(null);
+        mEgg10km.setBackground(null);
     }
 
     @Override
     public void onClick(View v) {
-        EggChooserFragment eggChooserFragment = new EggChooserFragment();
-        ((MainActivity) getActivity()).replaceFragment(eggChooserFragment);
+        ((MainActivity) getActivity()).replaceFragment(new EggOpenerFragment());
     }
 }
